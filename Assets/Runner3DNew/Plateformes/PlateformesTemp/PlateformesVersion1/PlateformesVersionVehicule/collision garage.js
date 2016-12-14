b6f4a4f24665cb5c00cbb144e93945d01de1181e
GeantTarse <ganemag2013@gmail.com>
2016-12-14 15:05:26 +0100

    script new

---
+++ Assets/Runner3DNew/Plateformes/PlateformesTemp/PlateformesVersion1/PlateformesVersionVehicule/collision garage.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function OnColliderEnter (col : Collision) {
+
+if(col.gameObject.tag == "Player"){
+
+Perdu();
+
+}
+
+}
+
+function Perdu(){
+yield WaitForSeconds(1.5f)
+
+Application.LoadLevel("perdusvoiture");
+
+}
